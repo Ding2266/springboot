65241318abb920a6be81e331b1932cc0d34c2466
Administrator <dj5827143@163.com>
2017-11-17 00:30:14 +0800

    模板渲染

--- microboot-deep/src/main/java/cn/mldn/microboot/controller/MessageController.java
+++ microboot-deep/src/main/java/cn/mldn/microboot/controller/MessageController.java
@@ -1,63 +1,20 @@
 package cn.mldn.microboot.controller;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import cn.mldn.microboot.service.IMessageService;
 
-@RestController	//本类中的所有的返回都是Rest类型（省略了"@ResponseBody"）
+@Controller
 public class MessageController {
-	@Resource 
-	private IMessageService messageService ; 
 	@Resource
-	private MessageSource messageSource ; 
-	
-	@RequestMapping("/msg")
-	public String hello() {
-		return this.messageService.echo() ; 
-	}
-	@RequestMapping("/welcome")
-	public String welcome() {
-		return this.messageSource.getMessage("welcome.msg", new Object[] {"mldn"},Locale.getDefault()) ;  
-	}
-	@RequestMapping("/echo")
-	@ResponseBody
-	public String echo(String msg) {
-		return "【ECHO返回】" + msg ;
- 	}
-	
-	@RequestMapping("/inner1")
-	@ResponseBody
-	public Object innerObject1(HttpServletRequest request,HttpServletResponse response) {
-		Map<String,String> map = new HashMap<String,String>() ; 
-		map.put("request-contextPath", request.getContextPath()) ; 
-		map.put("session-id", request.getSession().getId()) ; 
-		map.put("application-realPath", request.getServletContext().getRealPath("/")) ; 
-		map.put("response-contentType", response.getContentType()) ; 
-		return map ; 
-	}
-	@RequestMapping("/inner2")
-	@ResponseBody
-	public Object innerObject2() {
-		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest() ; 
-		HttpServletResponse response =((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse() ; 
-		Map<String,String> map = new HashMap<String,String>() ; 
-		map.put("request-contextPath", request.getContextPath()) ; 
-		map.put("session-id", request.getSession().getId()) ; 
-		map.put("application-realPath", request.getServletContext().getRealPath("/")) ; 
-		map.put("response-contentType", response.getContentType()) ; 
-		return map ; 
+	private IMessageService messageService ; 
+	@RequestMapping("/show")
+	public String show(Model model) { //传递参数
+		model.addAttribute("msg",this.messageService.echo()) ; 
+		return "message/message_show" ; 
 	}
 }
