c8e0213612917b1ce9dbaf593690b68c8b720da7
Administrator <dj5827143@163.com>
2017-11-16 20:37:01 +0800

    SpringBoot用junit类测试

---
+++ microboot-base/src/test/java/cn/mldn/microboot/base/bootStartTest.java
@@ -0,0 +1,24 @@
+package cn.mldn.microboot.base;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import junit.framework.TestCase;
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration  //现在要使用一个WEB程序启动
+@SpringBootTest(classes = bootStart.class)  //定义要测试的程序类
+public class bootStartTest {
+	@Resource
+	private bootStart bootstart ; 
+	@Test
+	public void testHome() {
+		String str = this.bootstart.home() ; 
+		System.err.println("bootstart.home()返回结果：" + str);
+		TestCase.assertEquals(str, "Hello World!");
+	}
+}
