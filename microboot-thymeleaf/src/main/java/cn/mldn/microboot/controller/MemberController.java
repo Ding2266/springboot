d8c4ac71f99b0d13ec0c60918a2fe94d08f4fa4b
Administrator <dj5827143@163.com>
2017-11-17 22:58:41 +0800

    逻辑处理操作

--- microboot-thymeleaf/src/main/java/cn/mldn/microboot/controller/MemberController.java
+++ microboot-thymeleaf/src/main/java/cn/mldn/microboot/controller/MemberController.java
@@ -1,6 +1,8 @@
 package cn.mldn.microboot.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,5 +23,21 @@ public class MemberController {
 		vo.setBirthday(new Date());
 		model.addAttribute("member", vo) ; 
 		return "back/member/member_show" ; 
+	}
+	@RequestMapping("/member_list")
+	public Object list(Model model) {
+		List<Member> all = new ArrayList<Member>() ; 
+		for(int x=0 ; x<10 ; x++) {
+			Member vo = new Member() ;
+			vo.setMid("dj" + x);
+			vo.setName("ding" + x);
+			vo.setDeptno(10L);
+			vo.setAge(10);
+			vo.setSalary(500.21);
+			vo.setBirthday(new Date());
+			all.add(vo) ; 
+		}
+		model.addAttribute("allMembers", all) ; 
+		return "back/member/member_list" ; 
 	}
 }
